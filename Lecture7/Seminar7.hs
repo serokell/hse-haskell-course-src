755c1d5c741022b3005e6ffe3854efe4acb35e3d
DanielRrr <daniel.rogozin@serokell.io>
2021-11-23 19:27:27 +0300

    code for the seminar

--- Lecture7/Seminar7.hs
+++ Lecture7/Seminar7.hs
@@ -2,6 +2,9 @@
 
 module Seminar7 where
 
+import Control.Monad
+import Control.Applicative
+
 toKleisli :: Monad m => (a -> b) -> a -> m b
 toKleisli f = return . f
 
@@ -95,6 +98,19 @@ getLastBook author = do
   book <- getBook author (reverse lib')
   return book
 
+{-
+(>>=) :: [a] -> (a -> [b]) -> [b]
+xs >>= k = concat (map k xs)
+
+xs :: [a]
+k :: a -> [b]
+
+concat :: forall a. [[a]] -> [a]
+
+map k xs :: [[b]]
+concat (map k xs) :: [b]
+-}
+
 cartesianProduct :: [a] -> [b] -> [(a, b)]
 cartesianProduct xs ys =
   xs >>= \x -> ys >>= \y -> return (x, y)
@@ -135,3 +151,221 @@ putStr'' = sequence_ . map putChar
 
 putStr''' :: String -> IO ()
 putStr''' = mapM_ putChar
+
+{-
+foo =
+  do { e1 ; e2 }
+
+foo = do
+  e1
+  e2
+
+foo' = e1 >>= \p -> e2
+
+foo'' =
+  do { p <- e1; e2 }
+
+foo''' = do
+  p <- e1
+  e2     -- p might occur in e2 somehow
+
+foo = let v = e1 in do e2
+
+foo' = do { let v = e1; e2 }
+
+foo'' = do
+  let v = e1
+  e2      -- v might occur in e2 somehow
+-}
+{-
+instance Monad IO where
+  return :: a -> IO a
+  return x = IO $ \w -> (w, x)
+
+  (>>=) :: IO a -> (a -> IO b) -> IO b
+  -- m :: IO a <-> RealWorld -> (RealWorld, a)
+  -- k :: a -> IO b <-> a -> (RealWorld -> (RealWorld, b))
+
+  -- we have to return IO b, that is,
+  -- RealWorld -> (RealWorld, b)
+
+  -- w :: RealWorld
+  -- m w :: (RealWorld, a)
+  -- (w', x) :: (RealWorld, a)
+  m >>= k = IO $ \w ->
+    case m w of
+      (w', x) -> k x w'
+-}
+
+newtype Reader r a = Reader { runReader :: r -> a }
+
+instance Functor (Reader r) where
+  fmap = undefined
+
+instance Applicative (Reader r) where
+  pure = undefined
+  (<*>) = undefined
+
+instance Monad (Reader r) where -- return :: a -> Reader r a return x = Reader $ const x
+      -- (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  f >>= k = Reader $ \e -> let v = runReader f e in runReader (k v) e
+
+ask :: Reader e e
+ask = Reader id
+
+asks ::(e -> a) -> Reader e a
+asks f = Reader f
+
+local :: (e -> b) -> Reader b a -> Reader e a
+local f m = Reader $ runReader m . f
+
+data Environment = Environment { ids  :: [Int]
+                               , name :: Int -> String
+                               , near :: Int -> (Int, Int) }
+
+inEnv :: Int -> Reader Environment Bool
+inEnv i = asks (elem i . ids)
+
+anyInEnv :: (Int, Int) -> Reader Environment Bool
+anyInEnv (i, j) = inEnv i ||^ inEnv j
+  where
+    (||^) = liftA2 (||)
+
+env = Environment [1..10] (\x -> show x) (\x -> (x - 1, x + 1))
+
+checkNeighbours :: Int -> Reader Environment (Maybe String)
+checkNeighbours i =
+  asks (`near` i) >>= \pair ->
+  anyInEnv pair   >>= \res  ->
+  if res
+  then Just <$> asks (`name` i)
+  else pure Nothing
+
+type Bindings = [(String,Int)]
+
+greeter :: Reader String String
+greeter = do
+  name <- ask
+  return ("hello, " ++ name ++ "!")
+
+ -- Returns True if the "count" variable contains correct bindings size.
+isCountCorrect :: Bindings -> Bool
+isCountCorrect bindings = runReader calc_isCountCorrect bindings
+
+ -- The Reader monad, which implements this complicated check.
+calc_isCountCorrect :: Reader Bindings Bool
+calc_isCountCorrect = do
+  count <- asks (lookupVar "count")
+  bindings <- ask
+  return (count == length bindings)
+
+lookupVar :: String -> Bindings -> Int
+lookupVar name bindings = maybe 0 id (lookup name bindings)
+
+sampleBindings = [("count",3), ("1",1), ("b",2)]
+
+action = do
+    putStr $ "Count is correct for bindings " ++ (show sampleBindings) ++ ": "
+    putStrLn $ show (isCountCorrect sampleBindings)
+
+calculateContentLen :: Reader String Int
+calculateContentLen = do
+  content <- ask
+  return (length content)
+
+-- Calls calculateContentLen after adding a prefix to the Reader content.
+calculateModifiedContentLen :: Reader String Int
+calculateModifiedContentLen = local ("Prefix " ++) calculateContentLen
+
+action' = do
+  let s = "12345";
+  let modifiedLen = runReader calculateModifiedContentLen s
+  let len = runReader calculateContentLen s
+  putStrLn $ "Modified 's' length: " ++ (show modifiedLen)
+  putStrLn $ "Original 's' length: " ++ (show len)
+
+newtype Writer w a = Writer { runWriter :: (a, w) }
+
+instance Functor (Writer w) where
+  fmap = undefined
+
+instance (Monoid w) => Applicative (Writer w) where
+  pure = undefined
+  (<*>) = undefined
+
+instance Monoid w => Monad (Writer w) where
+  Writer (x,v) >>= f = let (Writer (y, v')) = f x in Writer (y, v `mappend` v')
+
+tell :: Monoid w => w -> Writer w ()
+tell w = Writer ((), w)
+
+listen :: Monoid w => Writer w a -> Writer w (w, a)
+listen (Writer (a,w)) = Writer ((w,a),w)
+
+pass :: Monoid w => Writer w (a, w -> w) -> Writer w a
+pass (Writer ((a, f), w)) = Writer (a, f w)
+
+execWriter :: Writer w a -> w
+execWriter (Writer p) = snd p
+
+writer :: (a, w) -> Writer w a
+writer = Writer
+
+binPow :: Int -> Int -> Writer String Int
+binPow 0 _      = return 1
+binPow n a
+  | even n    = binPow (n `div` 2) a >>= \b ->
+                Writer (b * b, "Square " ++ show b ++ "\n")
+  | otherwise =
+      binPow (n - 1) a >>= \b ->
+      Writer
+        (a * b, "Mul " ++ show a ++ " and " ++ show b ++ "\n")
+
+type MyWriter = Writer [Int] String
+
+half :: Int -> Writer String Int
+half x = do
+        tell ("I just halved " ++ (show x) ++ "!")
+        return (x `div` 2)
+
+example :: MyWriter
+example  = do
+  tell [1..3]
+  tell [3..5]
+  return "foo"
+
+output :: (String, [Int])
+output = runWriter example
+
+deleteOn :: (Monoid w) => (w -> Bool) -> Writer w a -> Writer w a
+deleteOn p m = pass $ do
+  (w, a) <- listen m
+  case p w of
+    True -> return (a, id)
+    False -> return (a, const mempty)
+
+  -- Or pass alone
+deleteOn' :: (Monoid w) => (w -> Bool) -> Writer w a -> Writer w a
+deleteOn' p m = pass $ do
+  a <- m
+  return (a, (\w -> if p w then mempty else w))
+
+logTwo :: Writer [String] ()
+logTwo = do
+  deleteOn ((> 5) . length . head) $ tell ["foo"]
+  deleteOn ((> 5) . length . head) $ tell ["foobar"]
+
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+logNumber2 :: Int -> Writer [String] Int
+logNumber2 x = do
+  tell ["Got number: " ++ show x]
+  return x
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  tell ["multiplying " ++ show a ++ " and " ++ show b ]
+  return (a*b)
